80e426c1fdbf232ab27c261a0e5ba706c47b499c
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-04-14 11:36:31 -0700

    added validation to schema

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,28 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
-
+const dateFormat = require("../utils/dateFormat");
 
 //schema for pizza model, define the fields with specific data types
 const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal)
-
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
+      required: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
       default: "Large",
     },
     comments: [
@@ -32,7 +36,7 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
-      getters:true
+      getters: true,
     },
     id: false,
   }
@@ -40,7 +44,10 @@ const PizzaSchema = new Schema(
 // get total count of comments and replies on retrieval with virtuals
 PizzaSchema.virtual("commentCount").get(function () {
   //using reduce() to tally up every comment with replies
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
 // create the Pizza model using the PizzaSchema
--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -67,6 +67,8 @@ const handlePizzaSubmit = event => {
     })
     .catch(err => {
       console.log(err);
+      // instead of the app simply failing, the data will be stored in the new_pizza object store in the browser's IndexedDB database called pizza_hunts
+      saveRecord(formData);
     });
 };
 
